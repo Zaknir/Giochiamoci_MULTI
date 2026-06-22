6d45c6c7b4ee995fc1ae6e6206d20bed6ce1f251
Zaknir <48105790+Zaknir@users.noreply.github.com>
2026-06-22 15:19:46 +0200

    Add files via upload

---
+++ componenti.js
@@ -0,0 +1,74 @@
+// Definizione dell'Header (Titolo + Navigazione)
+class HeaderSito extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+<header>
+        <!-- Contenitore principale per affiancare Logo e Testi -->
+        <div class="header-main-box">
+            
+            <img src="logo_defcolori-sito.png" alt="Logo dell'associazione Giochiamoci! Un'illustrazione in stile fumetto con toni pastello che mostra due mani viola aperte verso l'alto per sorreggere diversi elementi ludici: un controller per videogiochi verde chiaro, carte da gioco gialle, un lucchetto azzurro con una chiave infilata (simbolo di escape room), un dado a venti facce rosa per giochi di ruolo e schede di gioco. In basso, la scritta in stampatello maiuscolo scuro 'GIOCHIAMOCI!'." class="logo">
+            
+            <!-- Sotto-oggetto verticale per i testi -->
+            <div class="testi-header">
+                <h1>Giochiamoci! APS</h1>
+                <p class="motto">Perché anche giocare è un diritto di tutti!</p>
+            </div>
+    
+        </div>
+
+        <!-- **QUESTO E' IL MENU DI NAVIGAZIONE** -->
+        <nav>
+            <div class="home-link-box">
+                <a href="index.html" id="home-link">Home</a>
+            </div>
+            <!-- SEZIONE: MENU PRINCIPALE -->
+            <details>
+                <summary>Chi siamo</summary>
+                <ul class="menu-principale">
+                    <li><a href="01_01_storia.html">Storia e valori</a></li>             
+                    <li><a href="01_02_sostienici.html">Sostienici</a></li>
+                    <li><a href="01_03_rete.html">Scopri la rete</a></li>
+                    <li><a href="01_04_contatti.html">Contatti</a></li>
+                </ul>
+            </details>
+        
+            <!-- SEZIONE: GIOCA CON NOI -->
+            <details>
+                <summary>Gioca con noi</summary>
+                <ul id="gioca-con-noi">
+                    <li><a href="02_01_escape.html">Escape Room</a></li>
+                    <li><a href="02_02_tavolo.html">Giochi da Tavolo</a></li>
+                    <li><a href="02_03_ruolo.html">Giochi di Ruolo</a></li> 
+                </ul>
+            </details>
+        
+            <!-- SEZIONE: PROGETTA ACCESSIBILE -->
+            <details>
+                <summary>Progetta Accessibile</summary>
+                <ul id="progetta-accessibile">
+                    <li><a href="03_01_accessibile.html">Capire l'accessibilità</a></li>
+                    <li><a href="03_02_videogiochi.html">Progettare videogiochi</a></li>
+                    <li><a href="03_03_acc_tavolo.html">Progettare giochi da tavolo</a></li>    
+                </ul>
+            </details>
+        </nav>
+
+    </header>
+    `;
+  }
+}
+
+// Definizione del Footer
+class FooterSito extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <footer>
+          <p>&copy; 2026 Giochiamoci! APS. All rights reserved.</p>
+      </footer>
+    `;
+  }
+}
+
+// Registrazione dei tag personalizzati
+customElements.define('header-sito', HeaderSito);
+customElements.define('footer-sito', FooterSito);
